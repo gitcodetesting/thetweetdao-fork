657df602449a14d66b8b348e8fae82266b182da3
Agus Friastana <friastana@gmail.com>
2022-04-20 13:01:18 +0800

    Update tweet.js

--- netlify/functions/tweet.js
+++ netlify/functions/tweet.js
@@ -34,7 +34,7 @@ exports.handler = async function (event, context) {
       accessSecret: tokenSecret,
     });
 
-    await client.v1.tweet(text);
+    await client.v2.tweet(text);
 
     return {
       statusCode: 200,
